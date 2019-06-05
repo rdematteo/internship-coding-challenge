4a178758c90641b7f1f3cddbceaa52208df01be5
Robert De Matteo <robertdematteo@hotmail.com>
2019-06-06 09:40:32 +1000

    added api get request into a function

--- http.rb
+++ http.rb
@@ -2,25 +2,39 @@ require 'sinatra'
 require 'sinatra/json'
 require 'http'
 
-url = 'https://dematteo.zendesk.com/api/v2/tickets.json'
-data = HTTP.basic_auth(:user => "robertdematteo@hotmail.com", :pass => "rob71ertd")
-.get(url).to_s
-data1 = JSON.parse(data)
+set :show_exceptions, :after_handler
+
+
+def getTicket
+  url = 'https://dematteo.zendesk.com/api/v2/tickets.json'
+  begin
+    data = HTTP.basic_auth(:user => "robertdematteo@hotmail.com", :pass => "rob71ertd")
+  .get(url).to_s
+  return data1 = JSON.parse(data)
+  rescue => exception
+    return 403, "Somethin"
+  end
+
+  error 403 do
+    "Access forbidden"
+    erb :oops
+  end
+
+end
+
 
 get '/tickets' do
+  data1 = getTicket()
   @data2 = data1['tickets'] 
   erb :index
 end
 
 get '/tickets/:id' do
+  data1 = getTicket()
   data3 = data1['tickets']
   id = params['id'].to_i
   @found_ticket = data3.find {|task| id == task['id'] }
 
-
-  # return json(found_ticket)
-
-
-erb :individual
+  erb :show
 end
 
