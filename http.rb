885a5d076f957f477deb3e7555142813a09c3d60
Robert De Matteo <robertdematteo@hotmail.com>
2019-06-05 10:26:10 +1000

    initial commit with ticket and api request completed

---
+++ http.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+# require 'sinatra/json'
+require 'http'
+
+get '/' do
+  
+  url = 'https://dematteo.zendesk.com/api/v2/tickets.json'
+  
+  data = HTTP.basic_auth(:user => "robertdematteo@hotmail.com", :pass => "rob71ertd")
+  .get(url).to_s
+
+  @data1 = JSON.parse(data)
+  
+
+  
+
+  erb :test
+end
+
+# data = HTTP.get(url).to_s
+# data_json = JSON.parse(data)
