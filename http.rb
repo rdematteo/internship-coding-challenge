4bc8975f2255c56f70033d276ede5c1b8989eeca
Robert De Matteo <robertdematteo@hotmail.com>
2019-06-05 12:52:27 +1000

    extra styling in individual page

--- http.rb
+++ http.rb
@@ -7,7 +7,6 @@ data = HTTP.basic_auth(:user => "robertdematteo@hotmail.com", :pass => "rob71ert
 .get(url).to_s
 data1 = JSON.parse(data)
 
-
 get '/tickets' do
   @data2 = data1['tickets'] 
   erb :index
@@ -16,10 +15,12 @@ end
 get '/tickets/:id' do
   data3 = data1['tickets']
   id = params['id'].to_i
-  found_ticket = data3.find {|task| id == task['id'] }
-  return json(found_ticket)
+  @found_ticket = data3.find {|task| id == task['id'] }
 
 
-erb :indivdual
+  # return json(found_ticket)
+
+
+erb :individual
 end
 
