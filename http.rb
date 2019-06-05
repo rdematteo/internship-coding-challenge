f5c47def774c47e67af6f42930b4d6004f62307f
Robert De Matteo <robertdematteo@hotmail.com>
2019-06-05 11:40:56 +1000

    individual tickets displayed with ref

--- http.rb
+++ http.rb
@@ -1,23 +1,25 @@
 require 'sinatra'
-# require 'sinatra/json'
+require 'sinatra/json'
 require 'http'
 
-get '/' do
-  
-  url = 'https://dematteo.zendesk.com/api/v2/tickets.json'
-  
-  data = HTTP.basic_auth(:user => "robertdematteo@hotmail.com", :pass => "rob71ertd")
-  .get(url).to_s
-  data1 = JSON.parse(data)
-  @data2 = data1['tickets']
+url = 'https://dematteo.zendesk.com/api/v2/tickets.json'
+data = HTTP.basic_auth(:user => "robertdematteo@hotmail.com", :pass => "rob71ertd")
+.get(url).to_s
+data1 = JSON.parse(data)
 
 
-  
-
-  
-
-  erb :test
+get '/tickets' do
+  @data2 = data1['tickets'] 
+  erb :index
 end
 
-# data = HTTP.get(url).to_s
-# data_json = JSON.parse(data)
+get '/tickets/:id' do
+  data3 = data1['tickets']
+  id = params['id'].to_i
+  found_ticket = data3.find {|task| id == task['id'] }
+  return json(found_ticket)
+
+
+erb :indivdual
+end
+
